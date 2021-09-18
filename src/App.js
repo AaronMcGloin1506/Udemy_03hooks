937621052ac142dc0f97febb3fa605d9c96adad8
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-09-18 17:40:43 +0100

    setting up the main files for routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Home from './components/home';
+import Profile from './components/profile';
+import Posts from './components/posts';
 
 
 const App = () => {
