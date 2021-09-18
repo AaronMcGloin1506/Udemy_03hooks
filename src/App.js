c5eb054126c4bb6d33487848d5b6b6894d665430
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-09-18 17:55:51 +0100

    Setting up the routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './components/home';
+import Post from './components/posts';
 import Profile from './components/profile';
-import Posts from './components/posts';
-
 
 const App = () => {
-  return(
-    <>
-      Hello
-    </>
-  )
+  return (
+    <BrowserRouter>
+        <header>
+          header
+        </header>
+        <Route path="/" exact component={Home}/>
+        <Route path="/posts" component={Post}/>
+        <Route path="/profile" component={Profile}/>
+    </BrowserRouter>
+  );
 }
 
 export default App;
