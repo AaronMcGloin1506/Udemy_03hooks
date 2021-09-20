751689444e8d14ef0b8de298c0f490d2411f06de
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-09-20 16:45:24 +0100

    Redirections

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react';
+import { Link, Redirect } from 'react-router-dom'
 
 
 const Profile = (props) => {
 
     console.log(props)
 
+    const [redir,setRedir] = useState(false);
+    console.log(props);
+
+    const redirect = () => {
+        if(redir){
+            props.history.push('/')
+            // return(
+            //     <Redirect to="/" />
+            // )
+        }
+    }
+
   return(
-    <>
-      <Link to={{
-          pathname:`${props.match.url}/posts`
-      }}>posts of profile</Link>
+    <> 
+        {redirect()}
+        <Link to={{
+            pathname:`${props.match.url}/posts`
+        }}>posts of profile</Link>
+        <hr />
+        <button onClick={()=>setRedir(true)}>Redirect</button>
     </>
   )
 }
